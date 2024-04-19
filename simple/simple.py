8a92290cfca877c4cd020afbbb77530752c22048
abordon <abordon@abastosoftware.ar>
2024-04-19 13:23:48 -0300

    Agrega logica para función simple_list en simple

--- simple/simple.py
+++ simple/simple.py
@@ -2,7 +2,14 @@ import random
 
 
 def simple_list():
-    pass
+    list_dict = []
+    for i in range(10):
+        dict = {
+            'id': i,
+            'age': random.randint(1, 100)
+        }
+        list_dict.append(dict)
+    return list_dict
 
 
 def sort_list():
